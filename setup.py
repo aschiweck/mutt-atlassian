50c73c37e69a0bd20bd75ccd6356b4255dd04b13
Andreas Schiweck <code@rainrider.de>
2017-05-14 00:29:19 +0200

    - ci setup

--- setup.py
+++ setup.py
@@ -24,6 +24,7 @@ setup(name='mutt-atlassian',
       zip_safe=False,
       install_requires=[
           'setuptools',
+          'pytest',
           # -*- Extra requirements: -*-
           'jira',
           'html2text',
